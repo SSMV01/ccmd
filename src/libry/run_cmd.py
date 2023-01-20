b2d373961bfb4385d3f9ab41a011e86e642afbd2
SSMV01 <shivaswaroopmv2@gmail.com>
2023-01-20 18:12:19 +0530

    multiprocessing for faster output writing

--- src/libry/run_cmd.py
+++ src/libry/run_cmd.py
@@ -1,6 +1,7 @@
 import sys
 import csv
 import logging
+import multiprocessing
 from datetime import datetime
 from utils import (rm_space)
 from utils import (compile_command, compile_command_for_output, compile_command_no_errs)
@@ -65,6 +66,8 @@ def write_output(csv_file):
 
                 for row in csvreader:
                     if usr_inp == row[1].strip():
+                        run_process = multiprocessing.Process(target=compile_command_no_errs, args=(row[0], ))
+                        run_process.start()
                         output_file = sys.argv[-1]
                         with open(output_file, 'a') as f:
                             f.writelines('\n')
@@ -75,7 +78,7 @@ def write_output(csv_file):
                             output = compile_command_for_output(row[0])
                             f.writelines(output + '\n')
                             f.writelines('-' * len(row[0]))
-                        compile_command_no_errs(row[0])
+                        run_process.join()
                         rowno += 1
                     else:
                         notfnd.append('e')
