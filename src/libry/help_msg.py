3e8b9d88ddf14ed4f78d565f812dcfe59576161b
SSMV01 <shivaswaroopmv2@gmail.com>
2023-02-09 17:11:27 +0530

    message refactor

--- src/libry/help_msg.py
+++ src/libry/help_msg.py
@@ -1,13 +1,11 @@
 import sys
 
 def help():
-    print("""Usage: ccmd [command(s)] [option(s)...]
-\nNote: Make sure to use "" (quotes) if your commands contain ' ' (space)
-OR you can use _ (underscores) for spaces.
-    \nType: ccmd --help [option name]
-For more information about the option.
+    print("""Basic Usage: ccmd [command(s)] [option(s)]
+    \nUsage: ccmd <option VALUE> [command(s)] [option(s)]
+    \nType: ccmd --help [option name]\nFor more information about the option.
     \nOptions:
-    -new          Create a custom new command.
+    --new          Create a custom new command.
     --opencsv     Open the cmds.csv file.
     --target      Set the location of the csv file.
     -o            Write output of command(s) to file AND execute them.
@@ -19,10 +17,9 @@ For more information about the option.
     \nSee: 'https://github.com/SSMV01/CustomCmd/blob/main/README.md' for more information.""")
     sys.exit(0)
 
-
-# For Later
+#For Later
 def new_help():
-    print("""Usage: ccmd -new
+    print("""Usage: ccmd --new
 Note: No other arguments are required.
         """)
 
