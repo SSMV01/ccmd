bae20da906edabfad042d20ebbbb3e3e2084dd71
SSMV01 <shivaswaroopmv2@gmail.com>
2023-02-21 22:05:49 +0530

    setcsv

--- ccmd/libry/__init__.py
+++ ccmd/libry/__init__.py
@@ -5,4 +5,4 @@ Functions for all the different arguments
 from .help_msg import help
 from .run_cmd import run_command
 from .create_cmd import create_command
-from .set_target import set_target_file
+from .set_csv import set_csv_file
---
+++ ccmd/libry/set_csv.py
@@ -0,0 +1,26 @@
+import sys
+import logging
+import os
+# Initialize logging
+logging.basicConfig(format="%(levelname)s: %(message)s")
+logging.getLogger().setLevel(logging.INFO)
+
+username = os.environ.get("LOGNAME")
+
+def set_csv_file(cmds_file):
+    cmds_file_location = cmds_file
+    if cmds_file_location == 'default':
+        cmds_file_location = f'/home/{username}/ccmd/bin/cmds.csv'
+
+    try:
+        with open(f'/home/{username}/ccmd/bin/cmds_target.txt', 'w', encoding='utf-8') as target_file:
+            target_file.write(cmds_file_location)
+        if cmds_file_location == "default":
+            logging.info("Default Set.")
+            sys.exit(0)
+        logging.info("Target File Set.")
+        sys.exit(0)
+
+    except PermissionError:
+        logging.error("Premission denied! Try using 'sudo'.")
+        sys.exit(2)
