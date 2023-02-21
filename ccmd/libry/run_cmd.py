9c21ff201594a00e51c766c5c2d9b55ad7236e96
SSMV01 <shivaswaroopmv2@gmail.com>
2023-02-21 22:11:13 +0530

    maxsplit, take commands as argument, code refactor, lazy formatting

--- ccmd/libry/run_cmd.py
+++ ccmd/libry/run_cmd.py
@@ -2,8 +2,7 @@ import sys
 import csv
 import logging
 from datetime import datetime
-from utils import (rm_space)
-from utils import (compile_command, compile_command_for_output)
+from utils import (rm_space, compile_command, compile_command_for_output)
 # Initialize logging
 logging.basicConfig(format="%(levelname)s: %(message)s")
 logging.getLogger().setLevel(logging.INFO)
@@ -15,41 +14,34 @@ def write_to_file(output_file: str, command_name: str, command: str):
     with open(output_file, 'a', encoding='utf-8') as file:
         file.writelines('\n')
         file.writelines(f'\nCommand {command_name}\n')
-        file.writelines(f'-' * 20)
-        file.writelines(f"\nStart time: {str(datetime.now()).split('.')[0]}\n")
+        file.writelines('-' * 20)
+        file.writelines(f"\nStart time: {str(datetime.now()).split('.', maxsplit=1)[0]}\n")
         file.writelines('\n')
         output = compile_command_for_output(command)
         file.writelines(output + '\n')
 
 
-def run_command(csv_file: str, o, oS):
+def run_command(commands, csv_file: str, output, output_silent):
     try:
-        if o or oS:
-            if len(sys.argv) <= 3:
-                logging.error("No commands OR file provided.")
-                sys.exit(2)
-            sys.argv.pop()
-            sys.argv.pop()
-
-        for arg in range(1, len(sys.argv)):
+        for command in commands:
             # Replaces space with _ (underscore)
-            usr_inp = rm_space(sys.argv[arg]) if ' ' in sys.argv[arg] else sys.argv[arg]
+            usr_inp = rm_space(command) if ' ' in command else command
             notfnd = []
             rowno = 0
 
-            with open(csv_file, 'r', encoding='utf-8') as f:
-                csvreader = csv.reader(f)
+            with open(csv_file, 'r', encoding='utf-8') as csvfile:
+                csvreader = csv.reader(csvfile)
 
                 for row in csvreader:
                     if len(row) < 2:
                         continue
                     if usr_inp == row[1].strip():
-                        if oS:
-                            o = None
-                            write_to_file(oS, row[1], row[0])
-                        elif o:
-                            oS = None
-                            write_to_file(o, row[1], row[0])
+                        if output_silent:
+                            output = False
+                            write_to_file(output_silent, row[1], row[0])
+                        elif output:
+                            output_silent = False
+                            write_to_file(output, row[1], row[0])
                             compile_command(row[0])
                         else:
                             compile_command(row[0])
@@ -58,16 +50,16 @@ def run_command(csv_file: str, o, oS):
                     rowno += 1
             # if (command not found) error in every row
             if len(notfnd) == rowno:
-                logging.error(f"Command '{sys.argv[arg]}' not found!")
+                logging.error("Command %s not found!", command)
                 logging.info("Use '--new' to create commands")
         sys.exit(0)
 
     except FileNotFoundError:
-        logging.error(f"{csv_file}: File Not Found!")
+        logging.error("%s: File Not Found!", csv_file)
         sys.exit(2)
 
     except IsADirectoryError:
-        logging.error(f"{csv_file}: File Not Found!")
+        logging.error("%s: File Not Found!", csv_file)
 
     except KeyboardInterrupt:
         logging.info("Exiting...")
