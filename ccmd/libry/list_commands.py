812c1e356b7ea39bf1003863a661f0c207ae7030
SSMV01 <shivaswaroopmv2@gmail.com>
2023-02-23 22:53:51 +0530

    change color

--- ccmd/libry/list_commands.py
+++ ccmd/libry/list_commands.py
@@ -12,7 +12,7 @@ def list_command_names(csv_file):
             if len(row) < 2:
                 continue
             if '?:' in row[0] or '?=' in row[0]:
-                print(Fore.BLUE + row[1])
+                print(Fore.LIGHTBLUE_EX + row[1])
             else:
                 print(row[1])
             row_no += 1
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-if chmod +x bin/ccmd.sh && cp bin/ccmd.sh ~/.local/bin/ccmd; then
+if chmod +x bin/ccmd.sh && cp bin/ccmd.sh ~/.local/bin/ccmd && pip install colorama; then
     echo Sucessfully initialized.
 else
     echo initialization failed!
