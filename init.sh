f71ba74e41d04b64bee48650cd47220a24328ca6
SSMV01 <shivaswaroopmv2@gmail.com>
2023-02-23 23:11:36 +0530

    Install dependencies

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 
-if chmod +x bin/ccmd.sh && cp bin/ccmd.sh ~/.local/bin/ccmd && pip install colorama; then
-    echo Sucessfully initialized.
+if chmod +x bin/ccmd.sh && cp bin/ccmd.sh ~/.local/bin/ccmd; then
+    if pip install colorama; then  
+        echo Sucessfully initialized.
+    else
+        echo Make sure pip is installed
+    fi
 else
     echo initialization failed!
 fi
