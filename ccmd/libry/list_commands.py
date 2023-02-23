ca9dae3d4020d8d118ee141a104fe9037ec764ca
SSMV01 <shivaswaroopmv2@gmail.com>
2023-02-23 18:53:46 +0530

    pylint refactory

--- ccmd/libry/list_commands.py
+++ ccmd/libry/list_commands.py
@@ -1,13 +1,13 @@
 import csv
 import colorama
-from colorama import *
+from colorama import Fore
 colorama.init(autoreset=True)
 
 def list_command_names(csv_file):
-    with open(csv_file, 'r') as csvfile:
+    with open(csv_file, 'r', encoding='utf-8') as csvfile:
         file_content = csv.reader(csvfile)
         row_no = 1
-        
+
         for row in file_content:
             if len(row) < 2:
                 continue
