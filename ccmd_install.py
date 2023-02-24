af68dad72ce601fe5de9147282ca10c19d996c29
SSMV01 <shivaswaroopmv2@gmail.com>
2023-02-24 15:44:33 +0530

    installation file

--- ccmd_install.py
+++ ccmd_install.py
@@ -1,21 +1,68 @@
-import os
 import sys
-import subprocess
+import logging
+from os import chdir, environ
+from subprocess import check_call, Popen, PIPE
+logging.basicConfig(format="%(levelname)s: %(message)s")
+logging.getLogger().setLevel(logging.INFO)
 
-USERNAME = os.environ.get('LOGNAME')
+USERNAME = environ.get('LOGNAME')
 
 print("CCMD")
 print('-' * 20)
 print("Starting installation")
 
-os.chdir(f'/home/{USERNAME}')
-subprocess.check_call('git clone https://github.com/ssmv01/ccmd', shell=True)
-subprocess.check_call('mv ccmd .ccmd', shell=True)
-os.chdir(f'/home/{USERNAME}/.ccmd')
-subprocess.check_call('chmod +x bin/ccmd.sh', shell=True)
-subprocess.check_call('cp bin/ccmd.sh ~/.local/bin/ccmd', shell=True)
-subprocess.check_call(f'{sys.executable} -m pip install colorama', shell=True)
+chdir(f'/home/{USERNAME}')
 
-print("\nInstallation complete")
+retry = 0
 
-subprocess.check_call('ccmd', shell=True)
+def install_dependencies():
+    with Popen([sys.executable, '-m', 'pip', 'install', 'colorama'], stdout=PIPE, stderr=PIPE) as install_colorama:
+        if b"No module named pip" in install_colorama.stderr.read():
+            if retry == 1:
+                logging.error("Failed to install pip")
+                logging.info("exiting...")
+                sys.exit(2)
+
+            logging.info("Installing pip...")
+            with Popen([ sys.executable ,'-m', 'ensurepip', '--upgrade'], stdout=PIPE, stderr=PIPE) as install_pip:
+                if install_pip.stderr.read() == None:
+                    logging.info("pip installed.")
+                    check_call(f'export PATH=/home/{USERNAME}/.local/bin:$PATH')
+                    retry += 1
+                    install_dependencies()
+                else:
+                    logging.error("Failed to install pip")
+                    logging.info("exiting...")
+                    sys.exit(2)
+        elif install_colorama.stderr:
+            print(install_colorama.stderr.read())
+        else:
+            with Popen(['echo', '$PATH'], stdout=PIPE) as verify_path:
+                if b'/home/{USERNAME}/.local/bin' not in verify_path.stdout.read():
+                    check_call(f'export PATH=/home/{USERNAME}/.local/bin:$PATH')
+            logging.info("Done.")
+
+logging.info("Installing dependencies...")
+install_dependencies()
+
+logging.info("Cloning ccmd...")
+try:
+    with Popen(['git', 'clone', 'https://github.com/ssmv01/ccmd'], stdout=PIPE, stderr=PIPE) as clone_ccmd:
+        if "Could not resolve host: github.com" in clone_ccmd.stderr.read():
+            logging.error("Failed to access github.com")
+        else:
+            logging.info("Done.")
+
+except FileNotFoundError as exception:
+    print(exception)
+
+check_call('mv ccmd .ccmd', shell=True)
+
+chdir(f'/home/{USERNAME}/.ccmd')
+
+check_call('chmod +x bin/ccmd.sh', shell=True)
+check_call('cp bin/ccmd.sh ~/.local/bin/ccmd', shell=True)
+
+logging.info("Installation complete\n")
+
+check_call('ccmd', shell=True)
