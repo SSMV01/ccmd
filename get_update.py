f01460e727a8d4a3419dd9eb96b6a54f505e8753
SSMV01 <shivaswaroopmv2@gmail.com>
2023-03-08 19:44:48 +0530

    Retain necessary files & comments

--- get_update.py
+++ get_update.py
@@ -7,9 +7,23 @@ USERNAME = os.environ.get('LOGNAME')
 
 os.chdir(f'/home/{USERNAME}')
 os.makedirs('ccmd_tmp_folder')
+
+# Copy necessary files
 os.system('cp .ccmd/get_ccmd.py ./ccmd_tmp_folder')
+os.system('cp .ccmd/bin/cmds.csv ./ccmd_tmp_folder')
+os.system('cp .ccmd/bin/cmds_target.txt ./ccmd_tmp_folder')
+
+# Remove folder
 os.system('rm -rf .ccmd')
-os.chdir(f'/home/{USERNAME}/ccmd_tmp_folder')
-os.system(f'{sys.executable} get_ccmd.py')
-os.chdir(f'/home/{USERNAME}')
+
+# Reinstall ccmd
+os.system(f'{sys.executable} ccmd_tmp_folder/get_ccmd.py')
+
+# Replace necessary files
+os.system('rm -rf .ccmd/bin/cmds.csv')
+os.system('rm -rf .ccmd/bin/cmds_target.txt')
+os.system('cp ccmd_tmp_folder/cmds.csv .ccmd/bin')
+os.system('cp ccmd_tmp_folder/cmds_target.txt .ccmd/bin')
+
+# Remove tmp folder
 os.system('rm -rf ccmd_tmp_folder')
