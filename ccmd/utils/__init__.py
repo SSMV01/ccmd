679dbd5e36b86b63fb6201aa1d822a1c252062f9
SSMV01 <shivaswaroopmv2@gmail.com>
2023-04-05 22:42:20 +0530

    compiler -> parser (change filename)

--- ccmd/utils/__init__.py
+++ ccmd/utils/__init__.py
@@ -2,5 +2,5 @@
 All the required utilities for ccmd
 """
 
-from .get_input import (get_the_input, nospace_input, rm_space)
-from .compiler import (compile_command, compile_command_for_output)
+from .get_input import get_the_input, nospace_input, rm_space
+from .parser import compile_command, compile_command_for_output
