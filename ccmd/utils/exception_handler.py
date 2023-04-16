abffb92142bc1b875a7a696086b978aa197780a1
SSMV01 <shivaswaroopmv2@gmail.com>
2023-04-16 14:32:59 +0530

    update error messages

--- ccmd/utils/exception_handler.py
+++ ccmd/utils/exception_handler.py
@@ -10,23 +10,23 @@ def keyboard_interrupt():
 
 
 def file_not_found(file):
-    logging.error("%s: file not found", file)
+    logging.error("'%s': file not found", file)
     sys.exit(2)
 
 
 # run_cmd.py
 def command_not_found(command):
-    logging.error("%s: command not found", command)
+    logging.error("'%s': command not found", command)
 
 
 # compiler.py
 # (se = syntax error)
 def se_missing_run(command):
-    logging.error("%s: syntax error: missing |=|", command)
+    logging.error("'%s': syntax error: missing run:", command)
 
 
 def se_missing_if(command):
-    logging.error("%s: syntax error: missing if= or if:", command)
+    logging.error("'%s': syntax error: missing if= or if:", command)
 
 
 # get_input.py
