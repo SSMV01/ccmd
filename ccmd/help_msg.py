54775f3eac43dfe9a4b11e42e0fb620e7ad6fa50
SSMV01 <shivaswaroopmv2@gmail.com>
2023-05-21 19:23:34 +0530

    update help message

--- ccmd/help_msg.py
+++ ccmd/help_msg.py
@@ -2,17 +2,17 @@ import sys
 
 
 def help_message():
-    print("""Usage: ccmd <option VALUE> [command(s)] [option(s)]
+    print("""Usage: ccmd [option VALUE] /OR/ [command(s)] [option(s)]
+    \nTo open the commands.csv file use 'ccmd' without any arguments
     \nOptions:
-    --new                        Create a custom new command.
-    --opencsv                    Open the cmds.csv file.
-    --target                     Set the location of the csv file.
-    -o         --output          Write output of command(s) to file AND execute them.
-    -oS        --output-silent   Write output of command(s) to file.
-    -h         --help            Displays this message.
-    -v         --version         Display the version number.
-    --update                     Get the latest changes made to the repository.
-    --uninstall                  Uninstall ccmd.
-    \nSee 'manual' folder located in '.ccmd' folder for more information.
+    --new                         Create a custom new command
+    --setcsv                      Set the location of the csv file
+    -o          --output          Write output of command(s) to file AND execute them
+    -os         --output-silent   Write output of command(s) to file
+    -h          --help            Displays this message
+    -v          --version         Display the version number
+    --update                     Get the latest changes made to the repository
+    --uninstall                  Uninstall ccmd
+    \nSee 'manual' located in '~/.ccmd' for more information
     \nv0.4.4-alpha""")
     sys.exit(0)
