46146f0e0000315d88144260c978d4d86f3ff982
SSMV01 <shivaswaroopmv2@gmail.com>
2023-05-21 19:24:44 +0530

    remove 'logging' module & remove --opencsv option

--- ccmd/customcmd.py
+++ ccmd/customcmd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 import os
 import sys
-import logging
 import argparse
 
 from uninstall_ccmd import uninstall
@@ -11,10 +11,8 @@ from set_csv import set_csv_file
 from list_commands import list_command_names
 from create_cmd import create_command
 from run_cmd import run_command
+from utils import exception_handler
 
-# Initialize logging
-logging.basicConfig(format='%(levelname)s: %(message)s')
-logging.getLogger().setLevel(logging.INFO)
 
 # Initialize argparse
 parser = argparse.ArgumentParser(
@@ -30,7 +28,7 @@ try:
     with open(f'/home/{USERNAME}/.ccmd/bin/cmds_target.txt', 'r', encoding='utf-8') as target_file:
         csv_file = target_file.read()
 except FileNotFoundError:
-    logging.error("Could not find 'cmds_target' at /home/%s/.ccmd/bin/.", USERNAME)
+    exception_handler.print_err("could not find 'cmds_target' at /home/%s/.ccmd/bin/.", USERNAME)
     sys.exit(2)
 
 parser.add_argument('--uninstall', help="Uninstall ccmd", action='store_true')
@@ -38,11 +36,10 @@ parser.add_argument('--update', help="Update ccmd to latest release", action='st
 parser.add_argument('-h', '--help', help="Display help message", action='store_true')
 parser.add_argument('-v', '--version', help="Display version number", action='store_true')
 parser.add_argument('--setcsv', type=str, help="Set target csv file")
-parser.add_argument('--opencsv', help="Open your csv file", action='store_true')
 parser.add_argument('-l', '--list', help="List all commands names in csv", action='store_true')
 parser.add_argument('--new', help="Create new custom command", action='store_true')
 parser.add_argument('-o', '--output', type=str, help="Write output to file and execute the command")
-parser.add_argument('-oS', '--output-silent', type=str, help="Write output to file")
+parser.add_argument('-os', '--output-silent', type=str, help="Write output to file")
 options, commands = parser.parse_known_args()
 
 
@@ -60,11 +57,7 @@ def main():
         set_csv_file(options.setcsv)
 
     if csv_file.isspace() or csv_file == '':
-        logging.error("csv file not specified")
-        sys.exit(2)
-    elif options.opencsv:
-        os.system(f'editor {csv_file}')
-        sys.exit(0)
+        exception_handler.csv_not_specified()
     elif options.list:
         list_command_names(csv_file)
     elif options.new:
@@ -75,7 +68,10 @@ def main():
 
 
 if len(sys.argv) == 1:
-    help_message()
+    if csv_file.isspace() or csv_file == '':
+        exception_handler.csv_not_specified()
+    os.system(f'editor {csv_file}')
+    sys.exit(0)
 else:
     if __name__ == '__main__':
         main()
